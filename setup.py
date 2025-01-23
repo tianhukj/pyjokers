eb7580ad3b588910f1f34fe4b6cea58b250282d1
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-23 14:15:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyjokers',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     description='A simple joke generator',
     long_description=open('README.md').read(),
