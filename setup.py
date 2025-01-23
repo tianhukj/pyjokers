644f77dbc7d060f6e730a3e0a50c8e098c3d3da9
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-23 13:00:05 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyjokers',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A simple joke generator',
+    author='tianhukj',
+    author_email='tianhukj@outlook.com',
+    url='https://github.com/tianhukj/pyjokers',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # 当前没有依赖项
+    ],
+)
