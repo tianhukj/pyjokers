073f6d37d44e242ed8b6f82712804dd28e924c91
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-01-23 20:42:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+# 读取 README 文件内容
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pyjokers',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     description='A simple joke generator',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # 指定描述文件的格式
     author='tianhukj',
     author_email='tianhukj@outlook.com',
     url='https://github.com/tianhukj/pyjokers',
@@ -18,4 +23,9 @@ setup(
     install_requires=[
         # 当前没有依赖项
     ],
+    entry_points={
+        'console_scripts': [
+            'pyjoke=pyjokers.pyjokers:main',
+        ],
+    },
 )
